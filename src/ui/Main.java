471a99b09db8f26b0cb4de54aa00642cf0e85ae0
FernandaRojas152 <FernandaRojas152@hotmail.com>
2020-04-24 23:59:15 -0500

    Now the principal window in the game it's shown on screen, although right now it doesn't do anything. Also adding a line so I can change in the future the font-family of my label

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,16 +1,37 @@
 package ui;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
 /**
  * 
  * @author Fernanda
- *
+ * @version March 24th 2020
+ * Class Main
  */
 
-public class Main {
+public class Main extends Application{
 
 	public static void main(String[] args) {
-		
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		/**Label txtPoints = new Label("Points");
+		txtPoints.setFont(new Font("Bebas Neue", 40));
+		*/
+		Parent root=FXMLLoader.load(getClass().getResource("PrincipalWindow.fxml"));
+		Scene scene= new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("User profile");
+		primaryStage.setResizable(false);
+		primaryStage.show();
 	}
 
 }
---
+++ src/ui/UserController.java
@@ -0,0 +1,19 @@
+package ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class UserController {
+	@FXML
+	private Label txtPoints;
+
+	@FXML
+	private Label points;
+
+	@FXML
+	private Label nickname;
+
+	@FXML
+	private Label gender;
+
+}
