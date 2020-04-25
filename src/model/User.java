3b57740fe100d40537cd56ae32c415a63c964263
FernandaRojas152 <FernandaRojas152@hotmail.com>
2020-04-24 20:10:51 -0500

    Adding new Clases and interface of User's profile

--- src/model/User.java
+++ src/model/User.java
@@ -3,8 +3,64 @@ package model;
 /**
  * 
  * @author Fernanda
- *
+ * @version March 24th 2020
+ * Class User
  */
 public class User {
+	//Attributes
+	private String name;
+	private String nickname;
+	private String gender;
+	private String password;
+	
+	private Points points;
 
+	public User(String name, String nickname, String gender, String password) {
+		this.name = name;
+		this.nickname = nickname;
+		this.gender = gender;
+		this.password = password;
+		points= null;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Points getPoints() {
+		return points;
+	}
+
+	public void setPoints(Points points) {
+		this.points = points;
+	}
+	
 }
