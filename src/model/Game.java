4a249541497dd2b05c25236e2b10d419e045bc68
FernandaRojas152 <FernandaRojas152@hotmail.com>
2020-04-26 10:53:40 -0500

    Added two custom exceptions and userExists it's now working.

--- src/model/Game.java
+++ src/model/Game.java
@@ -3,9 +3,9 @@ package model;
 import java.util.ArrayList;
 
 /**
- * 
  * @author Fernanda
- * March 26th 2020
+ * @version March 26th 2020
+ * Class Game
  */
 public class Game {
 	
@@ -24,20 +24,35 @@ public class Game {
 	}
 	
 	/**
-	 * 
-	 * @param nickname
-	 * @return
+	 * This method will search if the userExists already has been registered and exists in the game.
+	 * @param nickname -name that the user has and it's going to be the one that it's going to be
+	 * used to verified.
+	 * @return exists -true if it exists and false if it doesn't
 	 */
 	public boolean userExists(String nickname) {
 		boolean exists= false;
-		
 		int initial=0;
 		int end= users.size()-1;
 		
 		while(initial<= end && !exists) {
 			
+			int mid= (initial+end)/2;
+			
+			if(users.get(mid).getNickname().equalsIgnoreCase(nickname)) {
+				exists= true;
+			}else if(users.get(mid).getNickname().compareToIgnoreCase(nickname)<0) {
+				
+				initial= mid+1;
+				
+			}else {
+				end= mid-1;
+			}
 		}
-		
 		return exists;
+	}
+	
+	public User searchUser() {
+		return null;
+		
 	}
 }
