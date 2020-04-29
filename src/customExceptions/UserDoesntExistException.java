481a87be719712e486328f4a29cb705a5970dd48
FernandaRojas152 <FernandaRojas152@hotmail.com>
2020-04-29 01:49:38 -0500

    Added all tests for GameTest for now, all of them test users: sort, search, add

--- src/customExceptions/UserDoesntExistException.java
+++ src/customExceptions/UserDoesntExistException.java
@@ -2,7 +2,12 @@ package customExceptions;
 
 public class UserDoesntExistException extends Exception{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public UserDoesntExistException(String name) {
-	super("User"+name+"doesn't exist in the game, please verify the name and try again.");
+	super("User "+name+" doesn't exist in the game, please verify the name and try again.");
 	}
 }
--- tests/GameTest.java
+++ tests/GameTest.java
@@ -13,7 +13,7 @@ class GameTest {
 	private Game game;
 	private User user;
 	
-	//Stages
+	/** STAGES */
 	public void setUpStage1() throws UserAlreadyExistsException, RequiredFieldsException {
 		game= new Game();
 		user= new User("Fernanda", "fernandarojas152", "feminine", "fernanda");
@@ -23,14 +23,12 @@ class GameTest {
 	
 	public void setUpStage2() throws UserAlreadyExistsException, RequiredFieldsException {
 		game= new Game();
-		user= new User(null, null, null, null);
-		game.addUser(user);
 	}
 	
 	public void setUpStage3() throws UserAlreadyExistsException, RequiredFieldsException {
 		game= new Game();
 		user= new User("Fernanda", "fernandarojas152", "feminine", "fernanda");
-		User user2= new User("Angelica","fernandarojas152", "femenine", "angelica");
+		User user2= new User("Angelica","angelica2013", "femenine", "angelica");
 		game.addUser(user);
 		game.addUser(user2);
 	}
@@ -39,23 +37,25 @@ class GameTest {
 		game= new Game();
 		user= new User("Fernanda", "fernandarojas152", "feminine", "fernanda");
 		User user2= new User("Angelica","angelica2013", "femenine", "angelica");
+		User user3= new User("Amanda","amandaR", "femenine", "angelica");
+		User user4= new User("Saru","saruhiko", "male", "angelica");
 		game.addUser(user);
 		game.addUser(user2);
+		game.addUser(user3);
+		game.addUser(user4);
 	}
 	
 	public void setUpStage5() throws UserAlreadyExistsException, RequiredFieldsException {
 		game= new Game();
-		user= new User(null, "fernandarojas152", null, "fernanda");
-		game.addUser(user);
+		user= new User("Fernanda", "fernandarojas152", "feminine", "fernanda");
 	}
 	
 	
-	//tests
+	/** TESTS */
 	@Test
 	void testUserExists() throws UserAlreadyExistsException, RequiredFieldsException {
 		setUpStage1();
 		try {
-			//User userP= new User("Amanda", "amandaRojas", "femenine", "amanda");
 			User user2= new User("Fernanda", "fernandarojas152", "feminine", "fernanda");
 			game.addUser(user2);
 			
@@ -72,6 +72,52 @@ class GameTest {
 		} catch (UserDoesntExistException e) {
 			assertTrue(true);
 		}
-		
+	}
+	
+	@Test
+	void testEmptyUser() throws UserAlreadyExistsException, RequiredFieldsException {
+		setUpStage2();
+		Assertions.assertThrows(UserDoesntExistException.class, () ->game.searchUser("fer"), "Should throw an exception");
+	}
+	
+	@Test
+	void testFillData() throws UserAlreadyExistsException, RequiredFieldsException {
+		setUpStage5();		
+		try {
+			User userP= new User("", "amandaRojas", "", "amanda");
+			game.addUser(userP);
+		} catch (RequiredFieldsException e) {
+			assertTrue(true);
+		}
+	}
+	
+	@Test
+	void testSortUser() throws UserAlreadyExistsException, RequiredFieldsException {
+		setUpStage4();
+		game.sortUsers();
+		//System.out.println(game.getUsers().get(0).getNickname());
+		assertEquals("angelica2013", game.getUsers().get(0).getNickname());
+	}
+	
+	@Test
+	void testSearchUser() throws UserAlreadyExistsException, RequiredFieldsException {
+		setUpStage3();
+		try {
+			User userT= game.searchUser("angelica2013");
+			assertEquals("angelica2013", userT.getNickname());
+		} catch (UserDoesntExistException e) {
+			e.getMessage();
+		}
+	}
+	
+	@Test
+	void testSearchUserWrong() throws UserAlreadyExistsException, RequiredFieldsException {
+		setUpStage3();
+		try {
+			User userT= game.searchUser("king");
+			assertEquals("king", userT.getNickname());
+		} catch (UserDoesntExistException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
